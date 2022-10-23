22be3dac9e70e22902de2ec8aabceedc18df86a5
Chris Nelson <ChrisNelson8277@users.noreply.github.com>
2022-10-23 15:39:34 -0400

    tons of fixes, lots os css restructures

--- src/components/InfoModal.js
+++ src/components/InfoModal.js
@@ -1,52 +1,67 @@
-import React from 'react'
+import React from "react";
 import { GoX } from "react-icons/go";
-import { IconContext } from 'react-icons/lib'
-
+import { IconContext } from "react-icons/lib";
+import ReactDOM from "react-dom";
 function infoModal(props) {
-  return (
-    <div className='modal-test'>
-        <div className='modal-header'>
-            <IconContext.Provider value={{
-                className: "exit-icon"  
-            }}>
-            <button className='modal-exit'><GoX onClick={() => {props.openModal(false)}} /></button>
+  return ReactDOM.createPortal(
+    <>
+      <div className="modal-background">
+        <div className="modal-test">
+          <div className="modal-header">
+            <IconContext.Provider
+              value={{
+                className: "exit-icon",
+              }}
+            >
+              <GoX
+                onClick={() => {
+                  props.openModal(false);
+                }}
+              />
             </IconContext.Provider>
-        </div>
-        <div className='item-container'>
-            <div className='item-name'>
-            <h1>{props.name}</h1>
+          </div>
+          <div className="item-container">
+            <div className="item-name">
+              <h1>{props.name}</h1>
             </div>
-            <br/>
-            <div className='modal-item'>
-                <img className='info-img' src={props.img} alt="liquidation"></img>
+            <br />
+            <div className="modal-item">
+              <img className="info-img" src={props.img} alt="liquidation"></img>
             </div>
             <div>
-                <table className='item-table'>
-                    <tbody className='item-table-body'>
-
-                    <tr>
-                <th scope='row'><h1>Price:</h1></th>
-                <td>{props.price}</td>
-                </tr>
-                <tr>
-                <th scope='row'><h1>Condition:</h1></th>
-                <td>{props.condition}</td>
-                </tr>
-                <tr>
-                <th scope="row"><h1>Category:</h1></th>
-                <td>{props.category}</td>
-                </tr>
-                    </tbody>
-                </table>
-                <h1>Product Details:</h1>
-                <div className='item-desc'>
-                <p className='item-desc2'>{props.description}</p>
-                
-                </div>
+              <table className="item-table">
+                <tbody className="item-table-body">
+                  <tr>
+                    <th scope="row">
+                      <h5>Price:</h5>
+                    </th>
+                    <td>{props.price}</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">
+                      <h5>Condition:</h5>
+                    </th>
+                    <td>{props.condition}</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">
+                      <h5>Category:</h5>
+                    </th>
+                    <td>{props.category}</td>
+                  </tr>
+                </tbody>
+              </table>
+              <div className="item-desc">
+                <h3>Product Details:</h3>
+                <p className="item-desc2">{props.description}</p>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
-  )
+      </div>
+    </>,
+    document.getElementById("portal")
+  );
 }
 
-export default infoModal
+export default infoModal;
