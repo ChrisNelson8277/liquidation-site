ac3e54376b7768b72082f735c173cd5c2ab73450
Chris Nelson <ChrisNelson8277@users.noreply.github.com>
2022-10-23 21:19:49 -0400

    checkout fixes

--- src/components/CheckoutNav.js
+++ src/components/CheckoutNav.js
@@ -1,19 +1,37 @@
-import { AppBar, Button, Toolbar, Typography } from '@mui/material'
-import React from 'react'
-import {useNavigate} from 'react-router-dom'
+import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const CheckoutNav = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <AppBar>
-        <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} onClick={() => {navigate("/")}}>
-                <Button sx={{ width: "15%", color: "black", backgroundColor: "white", '&:hover': {
-      backgroundColor: 'whitesmoke', boxShadow: "0 0 12px black"}}}>Home</Button>
-            </Typography>
-        </Toolbar>
+      <Toolbar sx={{ backgroundColor: "rgb(146, 44, 44);", color: "white" }}>
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{ flexGrow: 1 }}
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          <Button
+            sx={{
+              width: "15%",
+              color: "black",
+              backgroundColor: "white",
+              "&:hover": {
+                backgroundColor: "whitesmoke",
+                boxShadow: "0 0 12px black",
+              },
+            }}
+          >
+            Home
+          </Button>
+        </Typography>
+      </Toolbar>
     </AppBar>
-  )
-}
+  );
+};
 
-export default CheckoutNav
+export default CheckoutNav;
